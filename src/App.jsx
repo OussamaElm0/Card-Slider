66b14c73bd0f5a6c0732804445839de920bb7469
Oussama El Mabrouki <138411649+OussamaElm0@users.noreply.github.com>
2023-11-06 22:58:59 +0100

    Add pictures to cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,66 @@
-import React from 'react'
+import React from "react";
 import { EffectCards } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/effect-cards";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css"
+import "./App.css";
+import images from "./Data";
 
-function Content({content}){
-    return (
-        <div className='content'>
-            <h1 className='text-light'>{content}</h1>
-        </div>
-    )
+function Content(content) {
+  const { name, url } = content;
+
+  const handleClick = () => {
+    console.log(`${name} was clicked}`);
+  };
+
+  return (
+    <div className="content card">
+      <img src={url} alt={name} className="card-image" />
+      <div className="card-body">
+        <h2 className="card-title">{name}</h2>
+        <p className="card-text">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
+          voluptatum.
+        </p>
+        <button className="btn btn-outline-success" onClick={handleClick}>Click Me</button>
+      </div>
+    </div>
+  );
 }
 
 export default function App() {
-    const contentArr = [
-      "Slide 1",
-      "Slide 2",
-      "Slide 3",
-      "Slide 4",
-      "Slide 5",
-      "Slide 6",
-      "Slide 7",
-      "Slide 8",
-      "Slide 9",
-      "Slide 10",
-    ];
- 
-    return (
-      <>
-        <Swiper
-          effect={"cards"}
-          grabCursor={true}
-          modules={[EffectCards]}
-          className="swiper"
-        >
-          {contentArr.map(slideContent => {
-            return (
-              <SwiperSlide>
-                <Content content={slideContent} />
-              </SwiperSlide>
-            );
-          })}
-        </Swiper>
-      </>
-    );
+  const contentArr = [
+    "Slide 1",
+    "Slide 2",
+    "Slide 3",
+    "Slide 4",
+    "Slide 5",
+    "Slide 6",
+    "Slide 7",
+    "Slide 8",
+    "Slide 9",
+    "Slide 10",
+  ];
+
+  return (
+    <>
+      <Swiper
+        effect={"cards"}
+        grabCursor={true}
+        modules={[EffectCards]}
+        className="swiper"
+      >
+        {images.map((slideContent, index) => {
+          const { name, url } = slideContent;
+
+          return (
+            <SwiperSlide key={index}>
+              <Content url={url} name={name} />
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </>
+  );
 }
---
+++ src/Data.js
@@ -0,0 +1,27 @@
+const images = [
+  {
+    name: "image1",
+    url: "https://picsum.photos/200",
+  },
+  {
+    name: "image2",
+    url: "https://picsum.photos/200",
+  },
+  {
+    name: "image3",
+    url: "https://picsum.photos/200",
+  },
+  {
+    name: "image4",
+    url: "https://picsum.photos/200",
+  },
+  {
+    name: "image5",
+    url: "https://picsum.photos/200",
+  },
+  {
+    name: "image6",
+    url: "https://picsum.photos/200",
+  },
+];
+export default images;
